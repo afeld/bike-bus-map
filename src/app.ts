1d152dc74142c9e37e238fb9893e036e8d9c93a5
Aidan Feldman <aidan.feldman@gmail.com>
2025-02-16 11:38:40 -0500

    make simpler entry point

--- src/app.ts
+++ src/app.ts
@@ -1,36 +1,16 @@
-// https://developers.google.com/maps/documentation/javascript/load-maps-js-api#js-api-loader
-import { Loader } from "@googlemaps/js-api-loader";
-import { getSheetData } from "./mapper";
-import MapperMap from "./mapper_map";
+import { mapFromSheet } from "./mapper";
 
 const sheetId = "1_BGSepevkTl0xade-TrJISX5Bp6r5tsBDy5_XY2umwc";
 const apiKey = "AIzaSyChiRwOLGsaXitE3ZrgM2qIoPpZm1cBjPs";
 const range = "from Bike Bus World";
 
-const loader = new Loader({
-  apiKey,
-});
-
-const createMap = async () => {
+const run = async () => {
   const mapEl = document.getElementById("map");
   if (!mapEl) {
     throw new Error("Map element not found");
   }
 
-  const map = new MapperMap(loader);
-  await map.setup(mapEl);
-  return map;
-};
-
-const run = async () => {
-  const [map, buses] = await Promise.all([
-    createMap(),
-    getSheetData(sheetId, range, apiKey),
-  ]);
-
-  // wait for all markers to be added
-  await Promise.all(buses.map((bus) => map.addMarker(bus)));
-  map.recenter();
+  mapFromSheet(mapEl, sheetId, range, apiKey);
 };
 
 window.addEventListener("load", run);
--- src/mapper.ts
+++ src/mapper.ts
@@ -1,4 +1,7 @@
+// https://developers.google.com/maps/documentation/javascript/load-maps-js-api#js-api-loader
+import { Loader } from "@googlemaps/js-api-loader";
 import BikeBus from "./bike_bus";
+import MapperMap from "./mapper_map";
 
 export const getSheetData = async (
   sheetId: string,
@@ -15,4 +18,30 @@ export const getSheetData = async (
   const buses = rows.map((row) => BikeBus.fromRow(headers, row));
 
   return buses;
+};
+
+const createMap = async (mapEl: HTMLElement, apiKey: string) => {
+  const loader = new Loader({
+    apiKey,
+  });
+
+  const map = new MapperMap(loader);
+  await map.setup(mapEl);
+  return map;
+};
+
+export const mapFromSheet = async (
+  mapEl: HTMLElement,
+  sheetId: string,
+  range: string,
+  apiKey: string
+) => {
+  const [map, buses] = await Promise.all([
+    createMap(mapEl, apiKey),
+    getSheetData(sheetId, range, apiKey),
+  ]);
+
+  // wait for all markers to be added
+  await Promise.all(buses.map((bus) => map.addMarker(bus)));
+  map.recenter();
 };
